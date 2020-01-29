02d6caeffa15617f35c9eec4a11c0f8c516addad
Rawa08 <rawa_aref@hotmail.com>
2020-01-29 19:19:52 +0000

    adding cart app

---
+++ cart/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, reverse
+
+# Create your views here.
+def view_cart(request):
+    """Render all the content in the cart"""
+    return render(request, 'cart.html')
+
+def add_to_cart(request, id):
+    """add a quantity of the specified product to the cart"""
+    quantity=int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    request.session['cart'] = cart    
+
+    return redirect(reverse('cart_view'))
+        
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'accounts'
+    'products',
+    'accounts',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -125,4 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
