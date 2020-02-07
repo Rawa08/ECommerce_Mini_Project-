4e1c06af2f3cce6969043e1b7e91f9533a67ca79
Rawa08 <rawa_aref@hotmail.com>
2020-02-07 20:13:16 +0000

    add aws s3 path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import env
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ifo+_%5(p8e_dcjjfb1^5dkj%b!dybxw823(^ui_0$(f=#@bf5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'accounts',
     'cart',
     'checkout',
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -81,12 +84,20 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) 
+}
+
+
+
+
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -126,6 +137,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires':'Thu, 31 DEC 2099 20:00:00 GMT',
+    'CacheControl':'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'ecommerce-rawa'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = 'ecommerce-rawa.s3.amazonaws.com'
+
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
